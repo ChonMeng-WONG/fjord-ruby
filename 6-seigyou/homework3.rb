d5ccc91795186ba1fb1a7fcc7be37a8fcb684f30
ChonMeng-WONG <toshiaki1989@gmail.com>
2014-07-15 19:09:30 +0900

    indentを2つに変更

--- 6-seigyou/homework3.rb
+++ 6-seigyou/homework3.rb
@@ -12,11 +12,11 @@ last = gets.chomp.to_i
 # 開始の年が終わりの年に正しくなるまでループする
 while start != last + 1
 #うるう年の条件を検測し、結果を出力する
-	if (start % 4 == 0) && (start % 100 != 0) || (start % 400 == 0)
-		puts start.to_s + '年はうるう年でござる'
-	else
-		puts start.to_s + '年はうるう年でござらん'
-end
+  if (start % 4 == 0) && (start % 100 != 0) || (start % 400 == 0)
+    puts start.to_s + '年はうるう年でござる'
+  else
+    puts start.to_s + '年はうるう年でござらん'
+  end
 #開始の年の処理, 終わりの年になるまで1を足し続けていく
-start = start + 1
+  start = start + 1
 end
