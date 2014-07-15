f796996f7a9d991caee3419905220a571b99f81d
ChonMeng-WONG <toshiaki1989@gmail.com>
2014-07-15 13:59:02 +0900

    ruby-Chapter6課題1

---
+++ 6-seigyou/homework1.rb
@@ -0,0 +1,15 @@
+#"99本のビールが壁に..." 遠足などでよく歌われる古典的な童謡の歌詞、
+#"99 Bottles of Beer on the Wall" を 出力するプログラムを書いてみましょう。 
+
+number = 99
+
+while number != 0
+	puts number.to_s + " Bottles of beer on the wall"
+    puts number.to_s + " Bottles of beer"
+    puts "Take one down and pass it around"
+    number = number - 1
+    puts number.to_s + " Bottles of beer on the wall"
+    puts " "
+end
+
+puts '歌い切ったね！すばらしい〜'
