35611cf6c737dff5e14203021728e563babcfe1e
ChonMeng-WONG <toshiaki1989@gmail.com>
2014-07-15 19:07:15 +0900

    indentを2つに変更

--- 6-seigyou/homework2-2.rb
+++ 6-seigyou/homework2-2.rb
@@ -2,21 +2,21 @@
 puts 'マレフィセント：「可愛い坊やですね。こんにちは」'
 times = 0
 while times != 3 #3回連続BYEを入力するまでに続く
-	msg = gets.chomp #入力する
-	if msg == 'BYE'
-	puts 'マレフィセント：「なんだと！？？？」'
-	times = times + 1 #'BYE'入力したら1回カウントする
+  msg = gets.chomp #入力する
+  if msg == 'BYE'
+  puts 'マレフィセント：「なんだと！？？？」'
+  times = times + 1 #'BYE'入力したら1回カウントする
 
-	else
-	if msg.upcase == msg #全て大文字だと返事してくれる
-	year = (rand(21)+1500)
-	puts 'マレフィセント：「いやー、' + year.to_s + '年以来ないね〜！」'
-	times = 0 # 'BYEではないので、連続回数を0にリセットする'
+  else
+  if msg.upcase == msg #全て大文字だと返事してくれる
+  year = (rand(21)+1500)
+  puts 'マレフィセント：「いやー、' + year.to_s + '年以来ないね〜！」'
+  times = 0 # 'BYEではないので、連続回数を0にリセットする'
 
-	else
-	puts 'マレフィセント：「.......もっと大きな声で話しておくれ、坊や！！！」' #それ以外は全て返事してくれない
-	times = 0 # 'BYEではないので、連続回数を0にリセットする'
-	end
+  else
+  puts 'マレフィセント：「.......もっと大きな声で話しておくれ、坊や！！！」' #それ以外は全て返事してくれない
+  times = 0 # 'BYEではないので、連続回数を0にリセットする'
+  end
 end
 end
 
