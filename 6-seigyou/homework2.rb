7ad2ddc49ffbd13b2d78c7d0db75a3f53524f19b
ChonMeng-WONG <toshiaki1989@gmail.com>
2014-07-15 19:08:49 +0900

    indentを2つに変更

--- 6-seigyou/homework2.rb
+++ 6-seigyou/homework2.rb
@@ -4,12 +4,12 @@ puts '可愛い坊やですね。こんにちは'
 msg = gets.chomp
 
 while msg != 'BYE'#BYEを入力するまでに続く
-	if msg.upcase == msg #全て大文字だと返事してくれる
+  if msg.upcase == msg #全て大文字だと返事してくれる
     year = (rand(21)+1930)
     puts 'いやー、' + year.to_s + '年以来ないね〜！'
   else
     puts '.......もっと大きな声で話しておくれ、坊や！！！' #それ以外は全て返事してくれない
-	end
+  end
   print ' '
   msg = gets.chomp #再度入力
 end
