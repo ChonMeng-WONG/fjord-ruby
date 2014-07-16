a88651e6afe286657ec476a428ca6c38e2472a6a
ChonMeng-WONG <toshiaki1989@gmail.com>
2014-07-16 10:42:38 +0900

    ruby-Chapter7課題1

---
+++ 7-array/homework1.rb
@@ -0,0 +1,16 @@
+puts '氏名を入力してください'
+puts '最後はEnterだけの空行にしてください'
+# 配列names、要素nameとも空にする
+names = []
+name = 
+
+# 入力が空まで続きます。
+while name != ''
+	name = gets.chomp
+	names << name # 入力した要素を配列に入れる
+end
+
+print 'アルファベット順に並べます'
+puts names.sort # ソードメソッドでアルファベット順で並べる。
+
+
