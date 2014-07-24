828b4a1a42ece549d6d41544f1b5f30768460a0a
ChonMeng-WONG <toshiaki1989@gmail.com>
2014-07-24 17:16:26 +0900

    ruby-Chapter10課題1(クラス+メソッドで書いてみた)

--- 10-block/homework1.rb
+++ 10-block/homework1.rb
@@ -20,3 +20,37 @@ end
 gfClock do
     puts 'DONG!'
   end
+
+puts "以上はブロック"
+
+# -----------------------------------------------------
+# ---ブロックなし、Classで作ってみた。
+# ---でも、なぜブロックを使うのか、まだよくわからない。
+# ---見た目はそんなにかわらない？
+
+class Song
+def gfClock
+  cnt = 0 
+  now = Time.now.hour # 現時刻を取得
+  if now <= 12 # Time.now.hour は 0-23 になる、それを1-24に
+    now += 1 # さらに、1-24を、1-12,1-12にする
+  end
+  if now >= 13
+    now -= 12
+  end
+  while cnt != now # now回処理を繰り返す
+    sing
+    cnt += 1
+  end
+end
+ 
+def sing
+  puts 'DONG!'
+end
+end
+
+sang = Song.new
+sang.gfClock
+
+puts "以上はクラスとメソッドのみ"
+# ----------------------------------------------------------
